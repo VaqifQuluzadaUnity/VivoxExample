89184dd48c8c0108f56b22a541de74c0cd4255f6
VaqifQuluzadaUnity <vaqif.quluzada1998@gmail.com>
2020-10-10 13:08:46 +0400

    Voice Chat ready

---
+++ VivoxTestExample/Assets/Scripts/UserChatIdentifier.cs
@@ -0,0 +1,137 @@
+﻿using System.ComponentModel;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using VivoxUnity;
+
+/// <summary>
+/// Using this class we can set info of user visually to the UI.
+/// </summary>
+/// 
+public class UserChatIdentifier : MonoBehaviour
+{
+    #region Private variables
+    VivoxVoiceManager _voiceManagerInstance;        //Singleton reference of voice manager
+    #endregion
+
+    #region Serialized Private Variables
+
+    [SerializeField] TextMeshProUGUI PlayerNameText;           //The Text component where players name goes
+
+    [SerializeField] Image ChatStateImage;          //The Image where sprite states(Mute,unmute,speaking) will be shown
+
+    [SerializeField] Sprite MutedImage, SpeakImage, StopSpeakImage;     //Sprites for Chat States
+
+    #endregion
+
+    #region Public variables
+
+    public IParticipant Participant;    //Current Participant that is related with that chat identifier
+
+    #endregion
+
+    #region Getter_Setter Variables
+
+    private bool isMuted;
+
+    public bool IsMuted     //For controlling mute state of local and other users
+    {
+        get { return isMuted; }    //For security purpose we change values of isMuted with this GetterSetter method. 
+
+        private set
+        {
+            if (Participant.IsSelf) //if this is our local user
+            {
+                _voiceManagerInstance.AudioInputDevices.Muted = value;      //we set the mute value of our local user
+            }
+            else      //if connected player
+            {
+                if (Participant.InAudio)        //if user connected to voice
+                {
+                    Participant.LocalMute = value;  //we set the value for connecte player
+                }
+            }
+            isMuted = value;                        //We set the value to isMuted that we wil return
+            UpdateChatStateImage();                 //And change the chat state image
+        }
+    }
+
+    private bool isSpeaking;
+
+    public bool IsSpeaking      //Using this variable we will detect speaking
+    {
+        get { return isSpeaking; }
+
+        private set
+        {
+            if(ChatStateImage && !IsMuted)      //if chatStateImage isnt null and user isnt IsMuted
+            {
+                isSpeaking = value;             //We set the value to isSpeaking
+                UpdateChatStateImage();        //And change the chat state image
+            }
+        }
+    }
+
+    #endregion
+
+
+    #region Public Methods
+
+    /// <summary>
+    /// Using this method we will set the info about user to the UI user chat identifier
+    /// </summary>
+    /// <param name="participant"></param>
+    public void SetupUserChatIdentifier(IParticipant participant)
+    {
+        _voiceManagerInstance = VivoxVoiceManager.Instance;     //we set the reference of VoiceManager
+
+        Participant = participant;                              //Set the reference of our Participant
+
+        PlayerNameText.text = Participant.Account.DisplayName;  //Set the name of user to the UI User chat identifier
+
+        IsMuted = Participant.IsSelf ? _voiceManagerInstance.AudioInputDevices.Muted : Participant.LocalMute;   //Detect if user is muted or not
+
+        isSpeaking = Participant.SpeechDetected;                                            //Detect if user is speaking or not
+        GetComponentInChildren<Button>().onClick.AddListener(MuteButtonFunction);           //Add the Mute event to the mute button
+        Participant.PropertyChanged += onPropertyChanged;                                   //Add the event to our current user used for speak detection for local and other users
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    private void UpdateChatStateImage()
+    {
+        if (IsMuted)//If player is Muted
+        {
+            ChatStateImage.sprite = MutedImage;     //we set the chat state image to muted
+        }
+        else//is not muted
+        {
+            if (IsSpeaking)//and if speaking
+            {
+                ChatStateImage.sprite = SpeakImage;  //we set the chat state image to speak
+            }
+            else//if not speaking
+            {
+                ChatStateImage.sprite = StopSpeakImage; //we set the chat state image to non-speak.
+            }
+        }
+    }
+
+    private void MuteButtonFunction()       //for mute and unMute user
+    {
+        IsMuted = !IsMuted;
+    }
+
+    private void onPropertyChanged(object obj ,PropertyChangedEventArgs args)           //This event is triggered for our local user and all users in connected users
+    {
+        switch (args.PropertyName)
+        {
+            case "SpeechDetected":      //If our user is speaking the image will be changed in both our UI, and other users' UI
+                IsSpeaking = Participant.SpeechDetected;
+                break;
+        }
+    }
+    #endregion
+}
---
+++ VivoxTestExample/Assets/Scripts/UsersInChatController.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UIElements;
+using VivoxUnity;
+
+/// <summary>
+/// Using this class we can control and see the data of the users(their nickname, profile picture and etc) in the current channel.
+/// We will place this script in Content GameObject of Scroll view object.
+/// </summary>
+public class UsersInChatController : MonoBehaviour
+{
+    #region Private Variables
+
+    private const string LobbyChannelNameTemp = "General";          //This is the temp name for our current channel then we add a event whether channel changed will be triggered and change the Users in Chat.
+
+    private VivoxVoiceManager _voiceManagerInstance;            //Reference for Voice Manager
+
+    private Dictionary<ChannelId, List<UserChatIdentifier>> UserChatIdentDict = new Dictionary<ChannelId, List<UserChatIdentifier>>();  //Dictionary that contains channels and list of users of this each channel
+
+    #endregion
+
+
+    #region Serialized Variables
+
+    [SerializeField] GameObject UserChatIdentPrefab;                //We will instantiate this and set the user's image, mute , unmute state and profile pic over this UI element.
+
+    #endregion
+
+    #region MonoBehaviour Callbacks
+
+    private void Start()
+    {
+        _voiceManagerInstance = VivoxVoiceManager.Instance;     //Decleration of reference of Voice manager
+
+        _voiceManagerInstance.OnParticipantAddedEvent += onParticipantAdded;        //When an user added this event will be triggered in all users
+        _voiceManagerInstance.OnParticipantRemovedEvent += onParticipantRemoved;    //When an user removed this event will be triggered in all users
+        //_voiceManagerInstance.OnUserLoggedOutEvent += onUserLoggedOut;              //When an user logged out this event will be triggered in all users(we can add this if we have login ,chat menu in one scene)
+        if (_voiceManagerInstance && _voiceManagerInstance.ActiveChannels.Count > 0)    //if voice manager reference exists and we have active channels
+        {
+            IChannelSession currentChannelSession = _voiceManagerInstance.ActiveChannels.FirstOrDefault(ac => ac.Channel.Name == LobbyChannelNameTemp);     //We get the channel session of our current active channel
+            foreach(IParticipant userInChannel in _voiceManagerInstance.LoginSession.GetChannelSession(currentChannelSession.Channel).Participants)         //and in this channel for all users
+            {
+                UpdateUserIdentDictionary(userInChannel,userInChannel.ParentChannelSession.Channel,true);                                                   //We set visual UserChat identifiers
+            }
+        }
+    }
+
+    private void OnDisable()        //when any error happened or connection lost, for preventing any problem we remove events from our voiceManager
+    {
+        _voiceManagerInstance.OnParticipantAddedEvent -= onParticipantAdded;
+        _voiceManagerInstance.OnParticipantRemovedEvent -= onParticipantRemoved;
+        //_voiceManagerInstance.OnUserLoggedOutEvent -= onUserLoggedOut;            //we can add this if we have login ,chat menu in a single scene
+    }
+    #endregion
+
+
+    #region Public Methods
+    public void CleanChannelUsersVisually(ChannelId channel)        //In our scroll view using this method we reset the positions of user chat identifiers in current connected channel
+    {
+        RectTransform rt = GetComponent<RectTransform>();
+        rt.sizeDelta = new Vector2(0, UserChatIdentDict[channel].Count * 50);
+    }
+
+    //public void ClearAllUserIdentifiers()
+    //{
+    //    foreach(List<UserChatIdentifier> userList in UserChatIdentDict.Values)
+    //    {
+    //        foreach(UserChatIdentifier chatIdent in userList)
+    //        {
+    //            Destroy(chatIdent.gameObject);
+    //        }
+    //        userList.Clear();
+    //    }
+    //    UserChatIdentDict.Clear();
+    //}        
+/* This method is depend upon the onUserLoggedOut event*/
+#endregion
+
+
+#region Private Methods
+
+private void UpdateUserIdentDictionary(IParticipant participant, ChannelId channel, bool isNewUser)
+{
+    if (isNewUser)      //we check if the user is new user that is connected to channel(we set isNewUser value in onParticipantAdded event)
+    {
+        GameObject newUserIdentObject = Instantiate(UserChatIdentPrefab, gameObject.transform);     //Create a new visual User Chat Identifier
+        UserChatIdentifier newUserIdent = newUserIdentObject.GetComponent<UserChatIdentifier>();    //get the UserChatIdentifier component from GameObject
+        List<UserChatIdentifier> userChannelList;                                                   //That is the list where we add new user. decleration depends upon following situtations...
+
+        if (UserChatIdentDict.ContainsKey(channel))                                                 //If there is current connected channel name in our dictionary
+        {
+            UserChatIdentDict.TryGetValue(channel, out userChannelList);                            //Get the list of users from this channel and declare userChannelList as them
+            newUserIdent.SetupUserChatIdentifier(participant);                                      //Setup participant visually
+            userChannelList.Add(newUserIdent);                                                      //Add this participant to the list
+            UserChatIdentDict[channel] = userChannelList;                                           //Set this list as a key of channel in dict
+        }
+        else                                                                                        //if there is no channel name for the given parameter
+        {
+            userChannelList = new List<UserChatIdentifier>();                                       //Create a new list
+            newUserIdent.SetupUserChatIdentifier(participant);                                      //Setup the user
+            userChannelList.Add(newUserIdent);                                                      //add this user to new list
+            UserChatIdentDict.Add(channel, userChannelList);                                        //And create the value(list) to the current channel in Dictionary
+        }
+        CleanChannelUsersVisually(channel);                                                         //We set the users in correct positions
+    }
+
+    else// if user left the channel
+    {
+        if (UserChatIdentDict.ContainsKey(channel))         //if there is a channel in our dictionary
+        {
+            UserChatIdentifier RemovedUser = UserChatIdentDict[channel].FirstOrDefault(predicate => predicate.Participant.Account.Name == participant.Account.Name);        //We find the name of user in list that is the value of current channel
+            if (RemovedUser != null)            //if user exists
+            {
+                UserChatIdentDict[channel].Remove(RemovedUser);     //Remove it from dictionary channel
+                Destroy(RemovedUser.gameObject);                    //Destroy the visual User Chat Identifier of user
+                CleanChannelUsersVisually(channel);                 //Reset the UI
+            }
+            else                                                    //If some error happens
+            {
+                Debug.LogError("There is no channelIdentifier of this user");
+            }
+        }
+    }
+}
+
+#endregion
+
+#region Vivox Custom Callbacks
+void onParticipantAdded(string userName,ChannelId channel, IParticipant participant)        //When participant join the channel this event will be triggered by VivoxVoiceManager
+{
+    UpdateUserIdentDictionary(participant, channel, true);
+}
+
+void onParticipantRemoved(string userName, ChannelId channel, IParticipant participant)     //When participant left the channel this event will be triggered by VivoxVoiceManager
+    {
+    UpdateUserIdentDictionary(participant, channel, false);
+}
+#endregion
+}
